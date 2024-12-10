c3a695b743c035184f8d990e50646653a964b731
Kamil <kdnuriev@gmail.com>
2024-12-11 01:20:03 +0400

    minor fixes

--- src/ws/client.go
+++ src/ws/client.go
@@ -1,7 +1,16 @@
 package ws
 
 import (
+	"encoding/json"
+	"time"
+
 	"github.com/gorilla/websocket"
+	"go.uber.org/zap"
+)
+
+const (
+	pongWait     = time.Second * 10
+	pingInterval = (pongWait * 9) / 10
 )
 
 // ClientList is a map holding list of clients
@@ -11,14 +20,16 @@ type ClientList map[*Client]bool
 type Client struct {
 	connection *websocket.Conn
 	manager    *Manager
-	writer     chan []byte
+	writer     chan Event
+	logger     *zap.SugaredLogger
 }
 
 func NewClient(conn *websocket.Conn, manager *Manager) *Client {
 	return &Client{
 		connection: conn,
 		manager:    manager,
-		writer:     make(chan []byte),
+		writer:     make(chan Event),
+		logger:     manager.logger,
 	}
 }
 
@@ -27,25 +38,50 @@ func (c *Client) ReadMessages() {
 	//Graceful close of the connection
 	defer c.manager.RemoveClient(c)
 
+	//Set message size limit to 512 bytes
+	c.connection.SetReadLimit(512)
+
+	//Configure wait time for pong responses
+	if err := c.connection.SetReadDeadline(time.Now().Add(pongWait)); err != nil {
+		c.logger.Errorw("error setting pong wait time", "err", err)
+		return
+	}
+	//Configure handling pong responses
+	c.connection.SetPongHandler(c.PongHandler)
+
 	//Infinite loop
 	for {
 		mesageType, payload, err := c.connection.ReadMessage()
 		if err != nil {
-			c.manager.logger.Errorw("error reading message", "err", err)
+			c.logger.Errorw("error reading message", "err", err)
 		}
-		c.manager.logger.Infow("New message", "MessageType", mesageType, "Payload", payload)
-		// Hack to test that WriteMessages works as intended
-		// Will be replaced soon
-		for wsclient := range c.manager.clients {
-			wsclient.writer <- payload
+
+		var request Event
+		c.logger.Debugw("raw data", "data", string(payload))
+		if err := json.Unmarshal(payload, &request); err != nil {
+			c.logger.Errorw("error unmarshaling event", "err", err)
+			break
+		}
+
+		c.logger.Infow("New message", "MessageType", mesageType, "Payload", payload)
+
+		//Route the event
+		if err := c.manager.routeEvent(request, c); err != nil {
+			c.logger.Errorw("error handling message", "err", err)
 		}
 	}
 }
 
 // WriteMessages is run in a seperate goroutine and is used to write messages over websocket conneciton
 func (c *Client) WriteMessages() {
+	//Create new ticker
+	ticker := time.NewTicker(pingInterval)
+
 	//Gracefully remove the client
-	defer c.manager.RemoveClient(c)
+	defer func() {
+		c.manager.RemoveClient(c)
+		ticker.Stop()
+	}()
 
 	//Infinite loop
 	for {
@@ -59,12 +95,31 @@ func (c *Client) WriteMessages() {
 				}
 				return
 			}
-			//Write a regulat test message
-			if err := c.connection.WriteMessage(websocket.TextMessage, message); err != nil {
-				c.manager.logger.Errorw("error writing message:", err)
+
+			data, err := json.Marshal(message)
+			if err != nil {
+				c.logger.Errorw("error marshaling event", "err", err)
+				return
 			}
-			c.manager.logger.Infow("Message has been sent", "msg", message)
+			//Write a regular test message
+			if err := c.connection.WriteMessage(websocket.TextMessage, data); err != nil {
+				c.logger.Errorw("error writing message:", err)
+			}
+			c.logger.Infow("Message has been sent", "msg", message)
+
+		case <-ticker.C:
+			//Send the ping
+			if err := c.connection.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
+				c.logger.Errorw("error writing ping message", "err", err)
+				return
+			}
+
 		}
 	}
 
+}
+
+func (c *Client) PongHandler(pongMsg string) error {
+	//Current time + pong.Wait time
+	return c.connection.SetReadDeadline(time.Now().Add(pongWait))
 }
--- src/ws/manager.go
+++ src/ws/manager.go
@@ -1,29 +1,57 @@
 package ws
 
 import (
+	"errors"
+	"fmt"
 	"sync"
 
 	"github.com/gorilla/websocket"
 	"go.uber.org/zap"
 )
 
+var ErrEventNotSupported = errors.New("this event type is not supported")
+
 type Manager struct {
-	WSUpgrader websocket.Upgrader
-	clients    ClientList
-	mu         sync.RWMutex
-	logger     *zap.SugaredLogger
+	WSUpgrader    websocket.Upgrader
+	clients       ClientList
+	mu            sync.RWMutex
+	logger        *zap.SugaredLogger
+	eventHandlers map[string]EventHandler
 }
 
 func NewManager(logger *zap.SugaredLogger) *Manager {
-	return &Manager{
+	m := &Manager{
 		WSUpgrader: websocket.Upgrader{
 			ReadBufferSize:  1024,
 			WriteBufferSize: 1024,
 		},
-		clients: make(ClientList),
-		mu:      sync.RWMutex{},
-		logger:  logger,
+		clients:       make(ClientList),
+		mu:            sync.RWMutex{},
+		logger:        logger,
+		eventHandlers: make(map[string]EventHandler),
 	}
+	m.setUpEventHandlers()
+	return m
+}
+
+// for now doesnt makes sense will fix later
+func (m *Manager) setUpEventHandlers() {
+	m.eventHandlers[EventSendMessage] = func(e Event, c *Client) error {
+		fmt.Println("skibidi op op")
+		return nil
+	}
+}
+
+func (m *Manager) routeEvent(event Event, c *Client) error {
+	//If event is not present in the map throw an error
+	if handler, ok := m.eventHandlers[event.Type]; ok {
+		//Execute the handler
+		if err := handler(event, c); err != nil {
+			return err
+		}
+		return nil
+	}
+	return ErrEventNotSupported
 }
 
 func (m *Manager) AddClient(client *Client) {
